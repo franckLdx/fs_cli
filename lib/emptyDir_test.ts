eefab378c6dd925f760a4f6e6cf56dc3d6386534
Utilisateur <franck.ledoux.pro@gmail.com>
2020-06-28 22:26:14 +0200

    emptyDir: add tests

--- lib/emptyDir_test.ts
+++ lib/emptyDir_test.ts
@@ -1,5 +1,11 @@
 import { cleanTest } from "./tools/test/misc.ts";
-import { makeDirectory, assertExists } from "./tools/test/fs.ts";
+import {
+  makeDirectory,
+  assertExists,
+  makeFile,
+  makeDirectories,
+  makeFiles,
+} from "./tools/test/fs.ts";
 import {
   runProcess,
   checkProcess,
@@ -19,22 +25,22 @@ const getoutputMessages = (paths: string[], dryRun = false) =>
 
 Deno.test({
   only: false,
-  name: "empty: create a new dir",
+  name: "emptyDir: create a new dir",
   async fn() {
     let p: Deno.Process | undefined;
     try {
-      const dir = await makeDirectory();
-      const destDirPath = join(dir, "dest");
-      p = await runEmptyDirProcess({ paths: [destDirPath] });
+      const tmpDir = await makeDirectory();
+      const targetDirPath = join(tmpDir, "dest");
+      p = await runEmptyDirProcess({ paths: [targetDirPath] });
       await checkProcess(
         p,
         {
           success: true,
-          expectedOutputs: [getOutputMessage(destDirPath)],
+          expectedOutputs: [getOutputMessage(targetDirPath)],
           expectedErrors: [""],
         },
       );
-      await assertExists([destDirPath]);
+      await assertExists([targetDirPath]);
     } finally {
       await cleanTest(p);
     }
@@ -43,24 +49,24 @@ Deno.test({
 
 Deno.test({
   only: false,
-  name: "empty: create a new dir in dry mode: not created",
+  name: "emptyDir: create a new dir in dry mode: not created",
   async fn() {
     let p: Deno.Process | undefined;
     try {
-      const dir = await makeDirectory();
-      const destDirPath = join(dir, "dest");
+      const tmpDir = await makeDirectory();
+      const targetDirPath = join(tmpDir, "dest");
       p = await runEmptyDirProcess(
-        { paths: [destDirPath], options: optionsDry },
+        { paths: [targetDirPath], options: optionsDry },
       );
       await checkProcess(
         p,
         {
           success: true,
-          expectedOutputs: [getOutputMessage(destDirPath, true)],
+          expectedOutputs: [getOutputMessage(targetDirPath, true)],
           expectedErrors: [""],
         },
       );
-      await assertNotExists([destDirPath]);
+      await assertNotExists([targetDirPath]);
     } finally {
       await cleanTest(p);
     }
@@ -69,23 +75,23 @@ Deno.test({
 
 Deno.test({
   only: false,
-  name: "empty: create new dirs",
+  name: "emptyDir: create new dirs",
   async fn() {
     let p: Deno.Process | undefined;
     const destDirNames = [join("des1", "sub"), "des2"];
     try {
-      const dir = await makeDirectory();
-      const destDirPaths = destDirNames.map((dest) => join(dir, dest));
-      p = await runEmptyDirProcess({ paths: destDirPaths });
+      const tmpDir = await makeDirectory();
+      const targetDirPaths = destDirNames.map((dest) => join(tmpDir, dest));
+      p = await runEmptyDirProcess({ paths: targetDirPaths });
       await checkProcess(
         p,
         {
           success: true,
-          expectedOutputs: getoutputMessages(destDirPaths),
+          expectedOutputs: getoutputMessages(targetDirPaths),
           expectedErrors: [""],
         },
       );
-      await assertExists(destDirPaths);
+      await assertExists(targetDirPaths);
     } finally {
       await cleanTest(p);
     }
@@ -94,25 +100,89 @@ Deno.test({
 
 Deno.test({
   only: false,
-  name: "empty: create new dirs in dry mode: not created",
+  name: "emptyDir: create new dirs in dry mode: not created",
   async fn() {
     let p: Deno.Process | undefined;
     const destDirNames = [join("des1", "sub"), "des2"];
     try {
-      const dir = await makeDirectory();
-      const destDirPaths = destDirNames.map((dest) => join(dir, dest));
+      const tmpDir = await makeDirectory();
+      const targetDirPaths = destDirNames.map((dest) => join(tmpDir, dest));
       p = await runEmptyDirProcess(
-        { paths: destDirPaths, options: optionsDry },
+        { paths: targetDirPaths, options: optionsDry },
       );
       await checkProcess(
         p,
         {
           success: true,
-          expectedOutputs: getoutputMessages(destDirPaths, true),
+          expectedOutputs: getoutputMessages(targetDirPaths, true),
           expectedErrors: [""],
         },
       );
-      await assertNotExists(destDirPaths);
+      await assertNotExists(targetDirPaths);
+    } finally {
+      await cleanTest(p);
+    }
+  },
+});
+
+Deno.test({
+  only: false,
+  name: "emptyDir: empty existing dirs",
+  async fn() {
+    let p: Deno.Process | undefined;
+    const targetDirsName = [join("des1", "sub"), "des2"];
+    const targetFilesName = targetDirsName.flatMap(
+      (targetDirName) =>
+        ["file1", "file2"].map(
+          (targetFileName) => join(targetDirName, targetFileName),
+        ),
+    );
+    try {
+      const targetDirsPath = await makeDirectories(targetDirsName);
+      const targetFilsPath = await makeFiles(targetFilesName);
+      p = await runEmptyDirProcess({ paths: targetDirsPath });
+      await checkProcess(
+        p,
+        {
+          success: true,
+          expectedOutputs: getoutputMessages(targetDirsPath),
+          expectedErrors: [""],
+        },
+      );
+      await assertNotExists(targetFilsPath);
+    } finally {
+      await cleanTest(p);
+    }
+  },
+});
+
+Deno.test({
+  only: false,
+  name: "emptyDir: empty existing dirs in dry mode: nothing done",
+  async fn() {
+    let p: Deno.Process | undefined;
+    const targetDirsName = [join("des1", "sub"), "des2"];
+    const targetFilesName = targetDirsName.flatMap(
+      (targetDirName) =>
+        ["file1", "file2"].map(
+          (targetFileName) => join(targetDirName, targetFileName),
+        ),
+    );
+    try {
+      const targetDirsPath = await makeDirectories(targetDirsName);
+      const targetFilsPath = await makeFiles(targetFilesName);
+      p = await runEmptyDirProcess(
+        { paths: targetDirsPath, options: optionsDry },
+      );
+      await checkProcess(
+        p,
+        {
+          success: true,
+          expectedOutputs: getoutputMessages(targetDirsPath, true),
+          expectedErrors: [""],
+        },
+      );
+      await assertExists(targetFilsPath);
     } finally {
       await cleanTest(p);
     }
