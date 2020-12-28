f664a605a99774ba17e914006abe8f6e665a6787
Utilisateur <franck.ledoux.pro@gmail.com>
2020-12-28 13:21:06 +0100

    bump deps

--- deps.ts
+++ deps.ts
@@ -1,46 +1,39 @@
-export {
-  assert,
-} from "https://deno.land/std@0.81.0/testing/asserts.ts";
+export { assert } from "https://deno.land/std@0.82.0/testing/asserts.ts";
 
 export {
-  ensureDir as denoEnsureDir,
   copy as denoCopy,
+  emptyDir as denoEmptyDir,
+  ensureDir as denoEnsureDir,
   exists,
   walk,
-  emptyDir as denoEmptyDir,
-} from "https://deno.land/std@0.81.0/fs/mod.ts";
+} from "https://deno.land/std@0.82.0/fs/mod.ts";
 
 export type {
-  CopyOptions as DenoCopyOptions
-} from "https://deno.land/std@0.81.0/fs/mod.ts";
+  CopyOptions as DenoCopyOptions,
+} from "https://deno.land/std@0.82.0/fs/mod.ts";
 
 export {
-  isGlob,
-  globToRegExp,
-  join,
-  dirname,
   basename,
+  dirname,
+  globToRegExp,
+  isGlob,
+  join,
   SEP,
-} from "https://deno.land/std@0.81.0/path/mod.ts";
+} from "https://deno.land/std@0.82.0/path/mod.ts";
 
 export {
-  setup as setupLogger,
   getLogger,
-  LoggerConfig,
   handlers,
-} from "https://deno.land/std@0.81.0/log/mod.ts";
-export {
-  Logger,
-  LogRecord,
-} from "https://deno.land/std@0.81.0/log/logger.ts";
+  LoggerConfig,
+  setup as setupLogger,
+} from "https://deno.land/std@0.82.0/log/mod.ts";
+export { Logger, LogRecord } from "https://deno.land/std@0.82.0/log/logger.ts";
 
-export { BaseHandler } from "https://deno.land/std@0.81.0/log/handlers.ts";
+export { BaseHandler } from "https://deno.land/std@0.82.0/log/handlers.ts";
 
-export { getLevelByName } from "https://deno.land/std@0.81.0/log/levels.ts";
+export { getLevelByName } from "https://deno.land/std@0.82.0/log/levels.ts";
 
-export {
-  Command,
-} from "https://deno.land/x/cliffy@v0.14.3/command/mod.ts";
+export { Command } from "https://deno.land/x/cliffy@v0.14.3/command/mod.ts";
 
 export type {
   ICommandOption,
