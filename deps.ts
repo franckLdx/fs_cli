03e6fe4aa57eeedc6929ab0710e0834562c692ea
Utilisateur <franck.ledoux.pro@gmail.com>
2021-04-15 18:39:07 +0200

    update deps, uses std@0.93.0 & tested with deno 1.9.0

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { assert } from "https://deno.land/std@0.83.0/testing/asserts.ts";
+export { assert } from "https://deno.land/std@0.93.0/testing/asserts.ts";
 
 export {
   copy as denoCopy,
@@ -6,11 +6,11 @@ export {
   ensureDir as denoEnsureDir,
   exists,
   walk,
-} from "https://deno.land/std@0.83.0/fs/mod.ts";
+} from "https://deno.land/std@0.93.0/fs/mod.ts";
 
 export type {
   CopyOptions as DenoCopyOptions,
-} from "https://deno.land/std@0.83.0/fs/mod.ts";
+} from "https://deno.land/std@0.93.0/fs/mod.ts";
 
 export {
   basename,
@@ -19,24 +19,24 @@ export {
   isGlob,
   join,
   SEP,
-} from "https://deno.land/std@0.83.0/path/mod.ts";
+} from "https://deno.land/std@0.93.0/path/mod.ts";
 
 export {
   getLogger,
   handlers,
   LoggerConfig,
   setup as setupLogger,
-} from "https://deno.land/std@0.83.0/log/mod.ts";
-export { Logger, LogRecord } from "https://deno.land/std@0.83.0/log/logger.ts";
+} from "https://deno.land/std@0.93.0/log/mod.ts";
+export { Logger, LogRecord } from "https://deno.land/std@0.93.0/log/logger.ts";
 
-export { BaseHandler } from "https://deno.land/std@0.83.0/log/handlers.ts";
+export { BaseHandler } from "https://deno.land/std@0.93.0/log/handlers.ts";
 
-export { getLevelByName } from "https://deno.land/std@0.83.0/log/levels.ts";
+export { getLevelByName } from "https://deno.land/std@0.93.0/log/levels.ts";
 
-export { Command } from "https://deno.land/x/cliffy@v0.16.0/command/mod.ts";
+export { Command } from "https://deno.land/x/cliffy@v0.18.2/command/mod.ts";
 
 export type {
   ICommandOption,
-} from "https://deno.land/x/cliffy@v0.16.0/command/mod.ts";
+} from "https://deno.land/x/cliffy@v0.18.2/command/mod.ts";
 
 export { badge } from "https://deno.land/x/cli_badges@v0.1.1/mod.ts";
--- dev_deps.ts
+++ dev_deps.ts
@@ -1,8 +1,8 @@
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.83.0/testing/asserts.ts";
+} from "https://deno.land/std@0.93.0/testing/asserts.ts";
 
-export { ensureDir, ensureFile } from "https://deno.land/std@0.83.0/fs/mod.ts";
+export { ensureDir, ensureFile } from "https://deno.land/std@0.93.0/fs/mod.ts";
 
-export { blue, green, red } from "https://deno.land/std@0.83.0/fmt/colors.ts";
+export { blue, green, red } from "https://deno.land/std@0.93.0/fmt/colors.ts";
